0c91ae38fb88b3e87feb403ef8bce59afe487ad9
gegemonTV <vovakonki776@gmail.com>
2021-02-14 02:10:23 +0300

    Added registration and login

--- app/src/main/java/com/minerva/helpforward/RegisterActivity.java
+++ app/src/main/java/com/minerva/helpforward/RegisterActivity.java
@@ -1,15 +1,23 @@
 package com.minerva.helpforward;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -19,22 +27,64 @@ public class RegisterActivity extends AppCompatActivity {
     Button RegisterButton;
     ProgressBar Bar;
 
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
 
         Email = (EditText) findViewById(R.id.email_reg);
         Nickname = (EditText) findViewById(R.id.nickname_reg);
         Password = (EditText) findViewById(R.id.password_reg);
         RegisterButton = (Button) findViewById(R.id.register);
 
+        Bar = (ProgressBar) findViewById((R.id.register_bar));
+
         RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Bar.setVisibility(View.VISIBLE);
+                if (Email.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Email не может быть пустым!", Toast.LENGTH_LONG).show();
+                    Bar.setVisibility(View.GONE);
+                }else if (Password.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Пароль не может быть пустым!", Toast.LENGTH_LONG).show();
+                    Bar.setVisibility(View.GONE);
+                } else if(Nickname.getText().toString().equals((""))){
+                    Toast.makeText(getApplicationContext(), "Имя пользователя не может быть пустым!", Toast.LENGTH_LONG).show();
+                    Bar.setVisibility(View.GONE);
+                } else {
+                    mAuth.createUserWithEmailAndPassword(Email.getText().toString(), Password.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                task.getResult().getUser().updateProfile(new UserProfileChangeRequest.Builder().setDisplayName(Nickname.getText().toString()).build())
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()){
+                                                    Log.d("OK", "COOL!");
+                                                    Bar.setVisibility(View.GONE);
+                                                    // TODO: startActivity(new Intent(getApplicationContext(),));
+                                                } else {
+                                                    Toast.makeText(getApplicationContext(), "Registration failed.",
+                                                            Toast.LENGTH_SHORT).show();
+                                                    Bar.setVisibility(View.GONE);
+                                                }
+                                            }
+                                        });
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Registration failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                Bar.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+                }
             }
         });
     }
